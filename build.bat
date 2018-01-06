cdfa4c060ddbea5ea5a7f1f9e7b008ce1174be26
Matthew Hazlett <hazlema@gmail.com>
2018-01-05 21:28:56 -0500

    v1.1

--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ start nexe BitTerm.js -r ./utils.js -o binaries/BitTerm-win-x64.exe
 start nexe BitTerm.js -r ./utils.js -t win32-x86-8.6.0 -o binaries/BitTerm-win-x32.exe
 start nexe BitTerm.js -r ./utils.js -t macos-8.4.0 -o binaries/BitTerm-mac
 start nexe BitTerm.js -r ./utils.js -t linux-x64 -o binaries/BitTerm-linux-x64
-
+start nexe BitTerm.js -r ./utils.js -t linux-x86 -o binaries/BitTerm-linux-x86
 pause
